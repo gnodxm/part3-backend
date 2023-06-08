2a317c2845054e69f40e21ffe367d6a73e7271c0
gnodxm <gnodxm@gmail.com>
2023-06-08 17:27:59 +0700

    Structure code

--- index.js
+++ index.js
@@ -1,104 +1,110 @@
-require('dotenv').config()
-const express = require('express')
-const app = express()
-const cors = require('cors')
-app.use(cors())
+require('dotenv').config();
+const express = require('express');
 
+const app = express();
+const cors = require('cors');
 
-const Note = require('./models/note')
+app.use(cors());
+
+const Note = require('./models/note');
 
 const requestLogger = (request, response, next) => {
-  console.log('Method:', request.method)
-  console.log('Path:  ', request.path)
-  console.log('Body:  ', request.body)
-  console.log('---')
-  next()
-}
+  console.log('Method:', request.method);
+  console.log('Path:  ', request.path);
+  console.log('Body:  ', request.body);
+  console.log('---');
+  next();
+};
 
-app.use(express.json())
-app.use(express.static('build'))
-app.use(requestLogger)
+app.use(express.json());
+app.use(express.static('build'));
+app.use(requestLogger);
 
 app.get('/api/notes', (request, response) => {
-  Note.find({}).then(notes => {
-    response.json(notes)
-  })
-})
+  Note.find({}).then((notes) => {
+    response.json(notes);
+  });
+});
 
-app.post('/api/notes', (request, response) => {
-  const body = request.body
+app.post('/api/notes', (request, response, next) => {
+  const { body } = request;
 
   if (!body.content) {
-    return response.status(400).json({ 
-      error: 'content missing' 
-    })
+    return response.status(400).json({
+      error: 'content missing',
+    });
   }
 
   const note = new Note({
     content: body.content,
-    important: body.important || false
-  })
+    important: body.important || false,
+  });
 
-	note.save().then(savedNote => {
-		response.json(savedNote)
-	})
-})
+  note.save().then((savedNote) => {
+    response.json(savedNote);
+  })
+    .catch((error) => next(error));
+});
 
 app.get('/api/notes/:id', (request, response, next) => {
   Note.findById(request.params.id)
-		.then(note => {
-		if (note) {
-			response.json(note)
-		} else {
-			response.status(404).end
-		}
-	})
-		.catch(error => next(error))
-})
+    .then((note) => {
+      if (note) {
+        response.json(note);
+      } else {
+        response.status(404).end;
+      }
+    })
+    .catch((error) => next(error));
+});
 
 app.put('/api/notes/:id', (request, response, next) => {
-	const body = request.body
+  const { content, important } = request.body;
 
-	const note = {
-		content: body.content,
-		important: body.important
-	}
+  const note = {
+    content: body.content,
+    important: body.important,
+  };
 
-	Note.findByIdAndUpdate(request.params.id, note, {new: true})
-		.then(updatedNote => {
-			response.json(updatedNote)
-		})
-		.catch(error => next(error))
-})
+  Note.findByIdAndUpdate(
+    request.params.id,
+    { content, important },
+    { new: true, runValidators: true, context: 'query' },
+  )
+    .then((updatedNote) => {
+      response.json(updatedNote);
+    })
+    .catch((error) => next(error));
+});
 
-app.delete('/api/notes/:id', (request, response) => {
-	Note.findByIdAndRemove(request.params.id)
-		.then(result => {
-			response.status(204).end()
-		})
+app.delete('/api/notes/:id', (request, response, next) => {
+  Note.findByIdAndRemove(request.params.id)
+    .then((result) => {
+      response.status(204).end();
+    })
 
-	.catch(error => next(error))
-})
+    .catch((error) => next(error));
+});
 
 const unknownEndpoint = (request, response) => {
-  response.status(404).send({ error: 'unknown endpoint' })
-}
+  response.status(404).send({ error: 'unknown endpoint' });
+};
 
-app.use(unknownEndpoint)
-
+app.use(unknownEndpoint);
 
 const errorHandler = (error, request, response, next) => {
-	console.log(error.message)
-	if (error.name === 'CastError') {
-		return response.status(400).send({error: 'malformatted id'})
-	}
-	next (error)
-}
+  console.log(error.message);
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' });
+  } if (error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message });
+  }
+  next(error);
+};
 
-app.use(errorHandler)
+app.use(errorHandler);
 
-
-const PORT = process.env.PORT
+const { PORT } = process.env;
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
-})
+  console.log(`Server running on port ${PORT}`);
+});
